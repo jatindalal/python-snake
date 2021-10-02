08e3321894a2e3a9b0c500e5c25aeec86881d6b8
Jatin Dalal <jd.jatindalal@gmail.com>
2021-10-02 20:28:53 +0530

    Implement snake body and basic fruit block

--- main.py
+++ main.py
@@ -2,17 +2,36 @@ import pygame
 from pygame.locals import *
 import time
 
-class Snake:
+SIZE = 40
+
+class Fruit:
     def __init__(self, parent_screen) -> None:
+        self.image = pygame.image.load("fruit.jpg").convert()
+        self.parent_screen = parent_screen
+        self.x = SIZE * 3
+        self.y = SIZE * 3
+    
+    def draw(self):
+        self.parent_screen.blit(self.image, (self.x, self.y))
+
+        pygame.display.flip()
+
+
+class Snake:
+    def __init__(self, parent_screen, length) -> None:
+        self.length = length
         self.parent_screen = parent_screen
         self.block = pygame.image.load("block.jpg").convert()
-        self.x = 200
-        self.y = 200
+        self.x = [SIZE]*length
+        self.y = [SIZE]*length
         self.direction = "right"
 
     def draw(self):
-        self.parent_screen.fill((200, 200, 200))
-        self.parent_screen.blit(self.block, (self.x, self.y))
+        self.parent_screen.fill((0, 0, 0))
+
+        for _ in range(self.length):
+            self.parent_screen.blit(self.block, (self.x[_], self.y[_]))
+
         pygame.display.flip()
 
     def move_left(self):
@@ -28,14 +47,19 @@ class Snake:
         self.direction = "down"
     
     def crawl(self):
+
+        for i in range(self.length - 1, 0, -1):
+            self.x[i] = self.x[i - 1]
+            self.y[i] = self.y[i - 1]
+
         if self.direction == "up":
-            self.y -= 12
+            self.y[0] -= SIZE
         elif self.direction == "down":
-            self.y += 12
+            self.y[0] += SIZE
         elif self.direction == "left":
-            self.x -= 12
+            self.x[0] -= SIZE
         elif self.direction == "right":
-            self.x += 12
+            self.x[0] += SIZE
         self.draw()
 
 
@@ -44,8 +68,14 @@ class Game:
         pygame.init()
         self.surface = pygame.display.set_mode((640, 480))
         self.surface.fill((200, 200, 200))
-        self.snake = Snake(self.surface)
+        self.snake = Snake(self.surface, 10)
+        self.fruit = Fruit(self.surface)
         self.snake.draw()
+        self.fruit.draw()
+
+    def play(self):
+        self.snake.crawl()
+        self.fruit.draw()
 
     def run(self):
         running = True
@@ -64,8 +94,9 @@ class Game:
                         self.snake.move_right()
                 elif event.type == QUIT:
                     running = False
-            self.snake.crawl()
-            time.sleep(0.2)
+                
+            self.play()
+            time.sleep(0.3)
 
 if __name__ == "__main__":
     game = Game()
